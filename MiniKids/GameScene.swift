466c3ca13da13adbcd4d8ac6f611263d364014fa
Gül Karatas <123191066+GulKaratas@users.noreply.github.com>
2024-10-22 20:34:35 +0300

    Animation added

--- MiniKids/GameScene.swift
+++ MiniKids/GameScene.swift
@@ -7,82 +7,95 @@
 
 import SpriteKit
 import GameplayKit
+import Lottie
 
 class GameScene: SKScene {
     
-    private var label : SKLabelNode?
-    private var spinnyNode : SKShapeNode?
+    var animationTiger : LottieAnimationView!
+    
+    var animationFlyBalloon : LottieAnimationView!
     
     override func didMove(to view: SKView) {
         
-        // Get label node from scene and store it for use later
-        self.label = self.childNode(withName: "//helloLabel") as? SKLabelNode
-        if let label = self.label {
-            label.alpha = 0.0
-            label.run(SKAction.fadeIn(withDuration: 2.0))
-        }
+        // `SKView`'i güvenli şekilde unwrap ediyoruz
+        guard let skView = self.view else { return }
         
-        // Create shape node to use during mouse interaction
-        let w = (self.size.width + self.size.height) * 0.05
-        self.spinnyNode = SKShapeNode.init(rectOf: CGSize.init(width: w, height: w), cornerRadius: w * 0.3)
+        // Tiger Animasyonu
+        animationTiger = LottieAnimationView(name: "AnimationTiger") // Tiger JSON animasyonu
+        animationTiger.frame = CGRect(x: 0, y: 0, width: 300, height: 300)
+        animationTiger.center = CGPoint(x: skView.bounds.midX - 120, y: skView.bounds.midY + 50) // Pozisyonlama
+        animationTiger.loopMode = .loop
+        animationTiger.play()
         
-        if let spinnyNode = self.spinnyNode {
-            spinnyNode.lineWidth = 2.5
-            
-            spinnyNode.run(SKAction.repeatForever(SKAction.rotate(byAngle: CGFloat(Double.pi), duration: 1)))
-            spinnyNode.run(SKAction.sequence([SKAction.wait(forDuration: 0.5),
-                                              SKAction.fadeOut(withDuration: 0.5),
-                                              SKAction.removeFromParent()]))
-        }
+        // Animasyon görünümünü SKView'e ekle
+        skView.addSubview(animationTiger)
+        
+        // Balloon Fly Animasyonu
+        animationFlyBalloon = LottieAnimationView(name: "AnimationFlyBallon") // Balloon JSON animasyonu
+        animationFlyBalloon.frame = CGRect(x: 0, y: 0, width: 300, height: 300)
+        animationFlyBalloon.center = CGPoint(x: skView.bounds.midX + 120, y: skView.bounds.midY + 50) // Pozisyonlama
+        animationFlyBalloon.loopMode = .loop
+        animationFlyBalloon.play()
+        
+        // Balloon animasyonunu SKView'e ekle
+        skView.addSubview(animationFlyBalloon)
+        // Buton Oluşturma
+        let forwardButton = UIButton(type: .system)
+        forwardButton.frame = CGRect(x: 0, y: 0, width: 50, height: 50)
+        forwardButton.center = CGPoint(x: animationFlyBalloon.center.x, y: animationFlyBalloon.center.y) // Balonun merkezine yerleştirir
+        forwardButton.backgroundColor = UIColor.clear
+        
+        // İleri sarma simgesi
+        let forwardIcon = UIImage(systemName: "forward.fill")
+        forwardButton.setImage(forwardIcon, for: .normal)
+        
+        // Butona işlev ekle
+        forwardButton.addTarget(self, action: #selector(forwardButtonTapped), for: .touchUpInside)
+        
+        // SKView'e buton ekle
+        skView.addSubview(forwardButton)
+        
     }
     
+    @objc func forwardButtonTapped() {
+        // Butona basıldığında yapılacak işlemler
+        print("İleri sarma butonuna tıklandı!")
+    }
     
     func touchDown(atPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.green
-            self.addChild(n)
-        }
+        
     }
     
     func touchMoved(toPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.blue
-            self.addChild(n)
-        }
+        
     }
     
     func touchUp(atPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.red
-            self.addChild(n)
-        }
+        
     }
     
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
-        if let label = self.label {
-            label.run(SKAction.init(named: "Pulse")!, withKey: "fadeInOut")
-        }
         
-        for t in touches { self.touchDown(atPoint: t.location(in: self)) }
     }
     
     override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchMoved(toPoint: t.location(in: self)) }
+        
     }
     
     override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchUp(atPoint: t.location(in: self)) }
+        
     }
     
     override func touchesCancelled(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchUp(atPoint: t.location(in: self)) }
+        
     }
     
     
     override func update(_ currentTime: TimeInterval) {
-        // Called before each frame is rendered
+        
     }
+    
+    
+    
+    
 }
