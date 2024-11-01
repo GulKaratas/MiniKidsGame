720fa1bf45c5c3f4af8a228f901de29fbb2261e9
Gül Karatas <123191066+GulKaratas@users.noreply.github.com>
2024-11-01 19:26:17 +0300

    backButton added

--- MiniKids/view/NextScene.swift
+++ MiniKids/view/NextScene.swift
@@ -4,8 +4,10 @@ import SpriteKit
 class NextScene: SKScene {
     
     var buttons: [SKCropNode] = []
+    var backButton: UIButton!
     
     override func didMove(to view: SKView) {
+        
         
         // Add background
         let background = SKSpriteNode(imageNamed: "h96b_d11e_210608")
@@ -45,6 +47,43 @@ class NextScene: SKScene {
             buttons.append(button)
             addSoftPulseAnimation(to: button)
         }
+        
+        createBackButton()
+    }
+    
+    func createBackButton() {
+        guard let view = self.view else { return }
+        
+        // Butonu başlat
+        backButton = UIButton(type: .custom)
+        
+        // Buton görselini ayarla
+        backButton.setImage(UIImage(named: "backButton"), for: .normal)
+        
+        // Buton boyutunu ayarla ve yuvarlak yap
+        backButton.frame = CGRect(x: 20, y: 20, width: 50, height: 50)
+        backButton.layer.cornerRadius = 25
+        backButton.clipsToBounds = true
+        
+        // Butona tıklama işlevi ekle
+        backButton.addTarget(self, action: #selector(backButtonTapped), for: .touchUpInside)
+        
+        // Butonu görünüme ekle
+        view.addSubview(backButton)
+    }
+    @objc func backButtonTapped() {
+        // NextScene'e geri dön
+        let nextScene = NextScene(size: self.size)
+        nextScene.scaleMode = .aspectFill
+        self.view?.presentScene(nextScene, transition: SKTransition.fade(withDuration: 1.0))
+        
+        // Geçiş yapıldığında butonu görünümden kaldır
+        backButton.removeFromSuperview()
+    }
+    
+    override func willMove(from view: SKView) {
+        // Scene'den çıkarken butonun kaldırıldığından emin olun
+        backButton.removeFromSuperview()
     }
     
     func createRoundedButton(imageName: String, position: CGPoint, size: CGSize) -> SKCropNode {
