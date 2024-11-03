7f0142b95d2226a3c14932673fa8205c287478cf
Gül Karatas <123191066+GulKaratas@users.noreply.github.com>
2024-11-03 12:11:11 +0300

    ShapesScene page done

--- MiniKids/view/ShapesScene.swift
+++ MiniKids/view/ShapesScene.swift
@@ -8,9 +8,17 @@ class ShapesScene: SKScene {
     var dogruSesEfekti: SKAction!
     
     var currentImage: SKSpriteNode?
-    var imageNames = ["daireDunya", "kareBiskuvi", "ucgenCetvel", "yildiz1"]
+    var imageNames: [[String]] = [
+        ["daireDunya", "kareBiskuvi", "ucgenCetvel", "yildiz1"],
+        ["daireKurabiye", "kareCerceve", "ucgenKarpuz", "yildizCicek"],
+        ["daireMeyve", "kareCikolata", "ucgenLed", "yildizDeniz"],
+        ["daireSaat", "kareKutu", "ucgenLevha", "yildizKurabiye"],
+        ["daireFootball","kareTablo","ucgenPizza","yildizSet"]
+    ]
+    var currentCycle = 0
     var imagesNode: SKSpriteNode!
     var initialImagePosition: CGPoint?
+    var matchedShapesCount = 0
 
     override func didMove(to view: SKView) {
         geriButonuOlustur()
@@ -27,7 +35,7 @@ class ShapesScene: SKScene {
         arkaPlan.size = self.size
         addChild(arkaPlan)
         
-        let katman = SKSpriteNode(color: UIColor.white.withAlphaComponent(0.9), size: self.size)
+        let katman = SKSpriteNode(color: UIColor.white.withAlphaComponent(0.7), size: self.size)
         katman.position = CGPoint(x: self.size.width / 2, y: self.size.height / 2)
         katman.zPosition = 0
         addChild(katman)
@@ -53,10 +61,11 @@ class ShapesScene: SKScene {
         sesEfektleri["daire"] = SKAction.playSoundFileNamed("suDamlasi", waitForCompletion: false)
         sesEfektleri["yildiz"] = SKAction.playSoundFileNamed("suDamlasi", waitForCompletion: false)
         sesEfektleri["ucgen"] = SKAction.playSoundFileNamed("suDamlasi", waitForCompletion: false)
-        dogruSesEfekti = SKAction.playSoundFileNamed("suDamlasi2", waitForCompletion: false) // Doğru eşleşme sesi
+        dogruSesEfekti = SKAction.playSoundFileNamed("suDamlasi2", waitForCompletion: false)
     }
 
     func sekilleriSiraIleEkle() {
+        matchedShapesCount = 0
         let sekilBilgileri = [
             (isim: "kare", x: self.size.width * 0.25, y: self.size.height * 0.75),
             (isim: "yildiz", x: self.size.width * 0.75, y: self.size.height * 0.75),
@@ -90,10 +99,10 @@ class ShapesScene: SKScene {
     }
 
     func rastgeleResimEkleEfektli() {
-        if imageNames.isEmpty { return }
+        if imageNames[currentCycle].isEmpty { return }
         
-        let randomIndex = Int(arc4random_uniform(UInt32(imageNames.count)))
-        let imageName = imageNames.remove(at: randomIndex)
+        let randomIndex = Int(arc4random_uniform(UInt32(imageNames[currentCycle].count)))
+        let imageName = imageNames[currentCycle].remove(at: randomIndex)
         
         imagesNode = SKSpriteNode(imageNamed: imageName)
         imagesNode.position = CGPoint(x: self.size.width / 2, y: self.size.height / 2)
@@ -105,15 +114,13 @@ class ShapesScene: SKScene {
         let fadeInAction = SKAction.fadeIn(withDuration: 1.0)
         imagesNode.run(fadeInAction)
     }
-
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
         if let touch = touches.first {
             let location = touch.location(in: self)
-            if let node = atPoint(location) as? SKSpriteNode {
-                if node == imagesNode {
-                    currentImage = node
-                    initialImagePosition = node.position // Başlangıç konumunu kaydet
-                }
+            // Yalnızca imagesNode'a dokunulduğunda currentImage olarak ayarlıyoruz
+            if imagesNode.contains(location) {
+                currentImage = imagesNode
+                initialImagePosition = imagesNode.position
             }
         }
     }
@@ -121,6 +128,7 @@ class ShapesScene: SKScene {
     override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
         if let touch = touches.first, let imageNode = currentImage {
             let location = touch.location(in: self)
+            // Dokunulan konumu takip etmek için imagesNode'u hareket ettiriyoruz
             imageNode.position = location
         }
     }
@@ -128,10 +136,12 @@ class ShapesScene: SKScene {
     override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
         if let touch = touches.first, let imageNode = currentImage {
             let location = touch.location(in: self)
+            // Eşleşmeyi kontrol et
             checkForMatch(at: location, imageNode: imageNode)
         }
         currentImage = nil
     }
+
     
     func checkForMatch(at location: CGPoint, imageNode: SKSpriteNode) {
         let shapes = children.filter { $0 is SKSpriteNode && $0.zPosition == 1 }
@@ -141,11 +151,17 @@ class ShapesScene: SKScene {
             if shape.contains(location) {
                 let shapeName = shape.name ?? ""
                 if isMatching(shapeName: shapeName, imageName: imageNode.name) {
-                    print("Eşleşti!")
+                    print("Matched!")
                     shape.removeFromParent()
                     imageNode.removeFromParent()
-                    run(dogruSesEfekti) // Doğru eşleşme sesini çal
-                    rastgeleResimEkleEfektli()
+                    matchedShapesCount += 1
+                    run(dogruSesEfekti)
+                    
+                    if matchedShapesCount == 4 {
+                        startNextCycle()
+                    } else {
+                        rastgeleResimEkleEfektli()
+                    }
                     matched = true
                     break
                 }
@@ -153,35 +169,58 @@ class ShapesScene: SKScene {
         }
 
         if !matched {
+            // Shake animation for incorrect match
             let shakeAction = SKAction.sequence([
-                SKAction.moveBy(x: -10, y: 0, duration: 0.1),
-                SKAction.moveBy(x: 20, y: 0, duration: 0.1),
-                SKAction.moveBy(x: -20, y: 0, duration: 0.1),
-                SKAction.moveBy(x: 20, y: 0, duration: 0.1),
-                SKAction.moveBy(x: -10, y: 0, duration: 0.1),
-                SKAction.move(to: initialImagePosition ?? CGPoint.zero, duration: 0.2) // Eski konuma dön
+                SKAction.moveBy(x: -10, y: 0, duration: 0.05),
+                SKAction.moveBy(x: 20, y: 0, duration: 0.05),
+                SKAction.moveBy(x: -20, y: 0, duration: 0.05),
+                SKAction.moveBy(x: 20, y: 0, duration: 0.05),
+                SKAction.moveBy(x: -10, y: 0, duration: 0.05),
+                SKAction.run { imageNode.position = self.initialImagePosition ?? CGPoint(x: self.size.width / 2, y: self.size.height / 2) }
             ])
-            imageNode.run(shakeAction) // Yanlış eşleşme sallama ve geri dönme hareketi
-            print("Yanlış eşleşme!")
-        }
-    }
-    
-    func isMatching(shapeName: String, imageName: String?) -> Bool {
-        guard let imageName = imageName else { return false }
-        switch shapeName {
-        case "daire":
-            return imageName == "daireDunya"
-        case "kare":
-            return imageName == "kareBiskuvi"
-        case "ucgen":
-            return imageName == "ucgenCetvel"
-        case "yildiz":
-            return imageName == "yildiz1"
-        default:
-            return false
+            imageNode.run(shakeAction)
         }
     }
 
+
+    func startNextCycle() {
+           currentCycle = (currentCycle + 1) % imageNames.count
+           imageNames[currentCycle] = ["daireKurabiye", "kareCerceve", "ucgenKarpuz", "yildizCicek"]
+           
+           // Update the images for the new cycle
+           if currentCycle == 1 {
+               imageNames[currentCycle] = ["daireKurabiye", "kareCerceve", "ucgenKarpuz", "yildizCicek"]
+           } else if currentCycle == 2 {
+               imageNames[currentCycle] = ["daireMeyve", "kareCikolata", "ucgenLed", "yildizDeniz"]
+           } else if currentCycle == 3 {
+               imageNames[currentCycle] = ["daireSaat","kareKutu","ucgenLevha","yildizKurabiye"]
+           } else if currentCycle == 4 {
+               imageNames[currentCycle] = ["daireFootball","kareTablo","ucgenPizza","yildizSet"]
+           }
+           
+           matchedShapesCount = 0
+           
+           run(SKAction.wait(forDuration: 0.5)) { [weak self] in
+               self?.sekilleriSiraIleEkle()
+           }
+       }
+
+       func isMatching(shapeName: String, imageName: String?) -> Bool {
+           guard let imageName = imageName else { return false }
+           switch shapeName {
+           case "daire":
+               return imageName.contains("daire")
+           case "kare":
+               return imageName.contains("kare")
+           case "ucgen":
+               return imageName.contains("ucgen")
+           case "yildiz":
+               return imageName.contains("yildiz")
+           default:
+               return false
+           }
+       }
+   
     override func willMove(from view: SKView) {
         geriButonu.removeFromSuperview()
     }
